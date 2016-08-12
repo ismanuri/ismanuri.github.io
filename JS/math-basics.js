a5814e20e94c9e85a47c179985b0baccf072792f
ismanuri <ismanuri@hotmail.com>
2016-08-12 15:02:50 +0700

    math-basics

---
+++ JS/math-basics.js
@@ -0,0 +1,29 @@
+var x = 2
+var y = 3
+angkaPangkat = function (x,y){
+	return Math.pow(x,y);
+}
+angkaPangkat (x,y);
+
+
+var a = 9
+akarKuadrat  = function (a){
+		return Math.sqrt(a);
+}
+akarKuadrat (a);
+
+
+var d = 3
+var e = 4
+hipotenusa = function (d,e){
+	return Math.sqrt(d*d+e*e) + "cm";
+}
+hipotenusa(d,e);
+
+
+var s = 7
+luasLingkaran = function (s){
+	return Math.PI*s*s + "cm"
+}
+luasLingkaran(s);
+
