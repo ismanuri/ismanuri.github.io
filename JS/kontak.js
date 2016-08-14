e06f818ae6ff1344ab11b24d69e21344a6573be0
ismanuri <ismanuri@hotmail.com>
2016-08-14 20:44:22 +0700

    kontak

---
+++ JS/kontak.js
@@ -0,0 +1,24 @@
+DaftarKontak = new Object;
+
+var DaftarKontak = {
+	Isman:{
+		nama: "Ismanuri",
+		email: "isman@mail.com",
+		telepon: "08123456"
+	},
+	Joko:{
+		nama: "Jokojek",
+		email: "joko@mail.com",
+		telepon: "08134567"
+	}
+};
+console.log(DaftarKontak);
+
+function tambahKontak(nama, email, telepon){
+	object:{
+		this.nama = nama;
+		this.email = email;
+		this.telepon = telepon;
+	}
+}
+
