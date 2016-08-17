4e1a3a745d9d7ee0113b2ba54f41c6936ba7218a
ismanuri <ismanuri@hotmail.com>
2016-08-17 14:29:04 +0700

    m

--- JS/kontak.js
+++ JS/kontak.js
@@ -1,24 +1,40 @@
-DaftarKontak = new Object;
+var DaftarKontak = ({
+	nama: "Ismanuri",
+	email: "isman@mail.com",
+	telepon: "08123456"
+	});
 
-var DaftarKontak = {
-	Isman:{
-		nama: "Ismanuri",
-		email: "isman@mail.com",
-		telepon: "08123456"
-	},
-	Joko:{
-		nama: "Jokojek",
-		email: "joko@mail.com",
-		telepon: "08134567"
-	}
-};
-console.log(DaftarKontak);
 
-function tambahKontak(nama, email, telepon){
-	object:{
-		this.nama = nama;
-		this.email = email;
-		this.telepon = telepon;
-	}
+
+var Program = {};
+
+Program.tambahKontak = function(nama, email, telepon){
+	DaftarKontak.push({
+		nama:nama,
+		email:email,
+		telepon:telepon
+	})
+	console.log("--- Kontak baru ditambahkan ---");
 }
 
+
+Program.lihatSemua = function(){
+	console.log("--- List Kontak ---");
+	DaftarKontak.forEach(function(entry,i){
+		console.log(i + ". nama: " + entry.nama + "\n email: " + entry.email + "\n telepon: " + entry.telepon);
+	}
+		)
+	};
+
+Program.lihatKontak = function(nama){
+	console.log("--- Lihat Kontak ---");
+	DaftarKontak.forEach(function(entry, i){
+		if(entry.nama === nama){
+		console.log("nama: " + entry.nama + "\n email: " + entry.email + "\n telepon: " + entry.telepon );
+		}
+	});
+
+}
+Program.tambahKontak("Joko", "joko@mail.com", "0821212121");
+Program.lihatSemua();
+Program.lihatKontak("Joko");
